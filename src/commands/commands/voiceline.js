070cafe76cefe91177c776825329d156c4e92e93
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-03-27 01:53:29 -0400

    added mp3 files for each voiceline

--- src/commands/commands/voiceline.js
+++ src/commands/commands/voiceline.js
@@ -1,3 +1,5 @@
+const fs = require("fs/promises");
+
 module.exports = {
     data: {
         name: "voiceline",
@@ -6,31 +8,18 @@ module.exports = {
         "contexts": [0, 1, 2]
     },
     async execute(interaction) {
-
-        const voicelines = [
-            "Don't TOUCH ME!",
-            "Go eat an anvil.",
-            "I don't like you.",
-            "I HATE YOU!",
-            "I have a knuckle-sandwich with your name on it.",
-            "I'm full of hate.",
-            "I'm so hate-filled right now.",
-            "I want you to eat an anvil.",
-            "You're ill.",
-            "You're mentally ill.",
-            "You disgust me.",
-            "Your oven's on, you left your oven on.",
-            "You should burn.",
-            "You're a LOSER!",
-            "I don't wanna see you.",
-            "Excuse me?",
-            "Who do you think you are?",
-            "What's in your pocket? What did you just put in your pocket?",
-            "You would tell me if you stole something, right?",
-            "Jump off of a microwave."
-        ];
+        const voicelinesJsonData = await fs.readFile("data/voicelines.json", {encoding: "utf8"});
+        const voicelinesMap = JSON.parse(voicelinesJsonData);
+        const voicelines = voicelinesMap["voicelines"];
         const voiceline = voicelines[Math.floor(Math.random() * voicelines.length)];
-    
-        await interaction.reply({ content: `${voiceline}`, ephemeral: false });
+            
+        await interaction.reply({
+            content: `${voiceline.text}`,
+            files:[{
+                attachment: `sounds/${voiceline.path}`,
+                name: `${voiceline.path}`
+            }],
+            ephemeral: false
+        });
       },
 }
